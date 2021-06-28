e3f0e202bdf4ff9019205485e060c9f5285cd81f
Deepanshu <72961400+thetechguy01@users.noreply.github.com>
2021-06-28 03:43:15 -0400

    Update my.js

--- my.js
+++ my.js
@@ -14,6 +14,29 @@ window.onscroll = function() {
     scrollBtn.style.display = "none";
   }
 }
+
+// Side Navigation Menu Js
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+
+menuBtn.onclick = function() {
+  navBar.classList.add("active");
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+  body.style.overflowX = "hidden";
+  scrollBtn.style.pointerEvents = "none";
+}
+
+cancelBtn.onclick = function() {
+  navBar.classList.remove("active");
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+  body.style.overflowX = "auto";
+  scrollBtn.style.pointerEvents = "auto";
+}
+
 // Side Navigation Bar Close While We click On Navigation Links
 
 let navLinks = document.querySelectorAll(".menu li a");
@@ -25,3 +48,36 @@ for (var i = 0; i < navLinks.length; i++) {
   });
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  //  little hack to detect if the user is on ie 11
+  const isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+  // get all the links with an ID that starts with 'sectionLink'
+  const listOfLinks = document.querySelectorAll("a[href^='#sectionLink");
+  // loop over all the links
+  listOfLinks.forEach(function (link) {
+    // listen for a click
+    link.addEventListener('click',  () => {
+      // toggle highlight on and off when we click a link
+      listOfLinks.forEach( (link) => {
+        if (link.classList.contains('highlighted')) {
+          link.classList.remove('highlighted');
+        }
+      });
+      link.classList.add('highlighted');
+      // get the element where to scroll
+      let ref = link.href.split('#sectionLink');
+      ref = "#section" + ref[1];
+      // ie 11 does not support smooth scroll, so we will simply scroll
+      if (isIE11) {
+        window.scrollTo(0, document.querySelector(ref).offsetTop);
+      } else {
+        window.scroll({
+          behavior: 'smooth',
+          left: 0,
+          // top gets the distance from the top of the page of our target element
+          top: document.querySelector(ref).offsetTop
+        });
+      }
+    })
+  })
+})
