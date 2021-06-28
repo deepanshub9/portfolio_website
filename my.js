036c945bb2b6db094b12ea237a7d0a15884633ca
Deepanshu <72961400+thetechguy01@users.noreply.github.com>
2021-06-28 02:59:59 -0400

    Add files via upload

---
+++ my.js
@@ -0,0 +1,17 @@
+// Sticky Navigation Menu Js
+
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+
+let val;
+
+window.onscroll = function() {
+  if(document.documentElement.scrollTop > 20){
+    nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  }else{
+    nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
+  }
+}
+
